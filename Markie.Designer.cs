e680766d712b91a9d97671823bb2a3658bd7dba4
0001skgR1 <144510783+0001skgR1@users.noreply.github.com>
2024-07-12 14:54:12 +0400

    Markie
    v0.936

---
+++ Markie.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Markie
+{
+    partial class MNForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ContainBx = new System.Windows.Forms.PictureBox();
+            this.CtxMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CopyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.PasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TextCN = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ContainBx)).BeginInit();
+            this.CtxMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ContainBx
+            // 
+            this.ContainBx.Location = new System.Drawing.Point(0, 0);
+            this.ContainBx.Name = "ContainBx";
+            this.ContainBx.Size = new System.Drawing.Size(584, 301);
+            this.ContainBx.TabIndex = 0;
+            this.ContainBx.TabStop = false;
+            // 
+            // CtxMenu
+            // 
+            this.CtxMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CopyToolStripMenuItem,
+            this.PasteToolStripMenuItem,
+            this.ClearToolStripMenuItem,
+            this.modeToolStripMenuItem});
+            this.CtxMenu.Name = "CtxMenu";
+            this.CtxMenu.Size = new System.Drawing.Size(106, 92);
+            this.CtxMenu.Text = "Main Functions";
+            // 
+            // CopyToolStripMenuItem
+            // 
+            this.CopyToolStripMenuItem.Name = "CopyToolStripMenuItem";
+            this.CopyToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.CopyToolStripMenuItem.Text = "Copy";
+            this.CopyToolStripMenuItem.Click += new System.EventHandler(this.CopyToolStripMenuItem_Click);
+            // 
+            // PasteToolStripMenuItem
+            // 
+            this.PasteToolStripMenuItem.Name = "PasteToolStripMenuItem";
+            this.PasteToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.PasteToolStripMenuItem.Text = "Paste";
+            this.PasteToolStripMenuItem.Click += new System.EventHandler(this.PasteToolStripMenuItem_Click);
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.ClearToolStripMenuItem.Text = "Clear";
+            this.ClearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // modeToolStripMenuItem
+            // 
+            this.modeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageModeToolStripMenuItem,
+            this.textModeToolStripMenuItem});
+            this.modeToolStripMenuItem.Name = "modeToolStripMenuItem";
+            this.modeToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.modeToolStripMenuItem.Text = "Mode";
+            // 
+            // imageModeToolStripMenuItem
+            // 
+            this.imageModeToolStripMenuItem.Name = "imageModeToolStripMenuItem";
+            this.imageModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.imageModeToolStripMenuItem.Text = "Image Mode";
+            this.imageModeToolStripMenuItem.Click += new System.EventHandler(this.ImageModeToolStripMenuItem_Click);
+            // 
+            // textModeToolStripMenuItem
+            // 
+            this.textModeToolStripMenuItem.Name = "textModeToolStripMenuItem";
+            this.textModeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.textModeToolStripMenuItem.Text = "Text Mode";
+            this.textModeToolStripMenuItem.Click += new System.EventHandler(this.TextModeToolStripMenuItem_Click);
+            // 
+            // TextCN
+            // 
+            this.TextCN.Location = new System.Drawing.Point(0, 0);
+            this.TextCN.Name = "TextCN";
+            this.TextCN.Size = new System.Drawing.Size(584, 301);
+            this.TextCN.TabIndex = 1;
+            this.TextCN.Text = "";
+            // 
+            // MNForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 301);
+            this.Controls.Add(this.TextCN);
+            this.Controls.Add(this.ContainBx);
+            this.Name = "MNForm";
+            this.ShowIcon = false;
+            this.Text = "Markie";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.ContainBx)).EndInit();
+            this.CtxMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ContainBx;
+        private System.Windows.Forms.ContextMenuStrip CtxMenu;
+        private System.Windows.Forms.ToolStripMenuItem CopyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ClearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imageModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textModeToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox TextCN;
+    }
+}
+
---
+++ Markie.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Markie
+{
+    public partial class MNForm : Form
+    {
+        public MNForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.TopMost = true;
+            this.KeyDown += new KeyEventHandler(SetControlV);
+            this.Resize += new EventHandler(MNForm_Resize);
+            this.MouseDown += new MouseEventHandler(ContextMouseTake);
+            ContainBx.MouseDown += new MouseEventHandler(ContextMouseTake);
+            TextCN.MouseDown += new MouseEventHandler(ContextMouseTake);
+            ContainBx.SizeMode = PictureBoxSizeMode.Zoom;
+            TextCN.Visible = false;
+
+            ContainBx.Image = Bap;
+            image = Bap;
+        }
+        private Bitmap Bap
+        {
+            get
+            {
+                Bitmap bmp = new Bitmap(ContainBx.Width, ContainBx.Height);
+                bmp.SetPixel(0, 0, Color.FromArgb(0, 0, 0, 0));
+                return bmp;
+            }
+        }
+        #region Key&MouseControls
+        private Image image;
+        private void SetControlV(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    image = Clipboard.GetImage();
+                    ContainBx.Image = image;
+                }
+            }
+        }
+        private void ContextMouseTake(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                CtxMenu.Show(this, e.Location);
+            }
+        }
+        #endregion
+        #region Containers2ImgSetScaler
+        private void MNForm_Resize(object sender, EventArgs e)
+        {
+            //IMG Container
+            ContainBx.Size = GetScaledSize(image.Size, this.ClientSize);
+
+            if (ContainBx.Right > this.ClientSize.Width)
+            {
+                ContainBx.Left = this.ClientSize.Width - ContainBx.Width;
+            }
+            if (ContainBx.Bottom > this.ClientSize.Height)
+            {
+                ContainBx.Top = this.ClientSize.Height - ContainBx.Height;
+            }
+
+            //TEXT Container
+            if (TextCN.Right > this.ClientSize.Width)
+            {
+                TextCN.Left = this.ClientSize.Width - TextCN.Width;
+            }
+            if (TextCN.Bottom > this.ClientSize.Height)
+            {
+                TextCN.Top = this.ClientSize.Height - TextCN.Height;
+            }
+        }
+        #endregion
+        #region ImageSetScaler
+        private Size GetScaledSize(Size imageSize, Size contSize)
+        {
+            double ratioX = (double)contSize.Width / imageSize.Width;
+            double ratioY = (double)contSize.Height / imageSize.Height;
+            double ratio = Math.Min(ratioX, ratioY);
+            int newWidth = (int)(imageSize.Width * ratio);
+            int newHeight = (int)(imageSize.Height * ratio);
+            return new Size(newWidth, newHeight);
+        }
+        #endregion
+        #region Context Menu
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContainBx.Visible)
+            {
+                if (image != null)
+                {
+                    Clipboard.SetData(DataFormats.Bitmap, (Object)image);
+                }
+                else
+                {
+                    MessageBox.Show("Image Container does not contain 'Image'!");
+                }
+            }
+            else if (TextCN.Visible)
+            {
+                if (!String.IsNullOrEmpty(TextCN.Text))
+                {
+                    TextCN.SelectAll();
+                    Clipboard.SetText(TextCN.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Text Container does not contain 'Text'!");
+                }
+            }
+        }
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContainBx.Visible)
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    image = Clipboard.GetImage();
+                    ContainBx.Image = image;
+                }
+                else
+                {
+                    MessageBox.Show("Clipboard does not contain 'Image'!");
+                }
+            }
+            else if (TextCN.Visible)
+            {
+                if (Clipboard.ContainsText())
+                {
+                    TextCN.Text = Clipboard.GetText();
+                }
+                else
+                {
+                    MessageBox.Show("Clipboard does not contain 'Text'!");
+                }
+            }
+        }
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContainBx.Visible)
+            {
+                if (image != null)
+                {
+                    ContainBx.Image = Bap;
+                    image = Bap;
+                }
+            }
+            else if (TextCN.Visible)
+            {
+                if (!String.IsNullOrEmpty(TextCN.Text))
+                {
+                    TextCN.Clear();
+                }
+            }
+        }
+        private void ImageModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ContainBx.Visible == false)
+            {
+                ContainBx.Visible = true;
+                TextCN.Visible = false;
+            }
+            else
+            {
+                ContainBx.Visible = false;
+                TextCN.Visible = true;
+            }
+        }
+        private void TextModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (TextCN.Visible == false)
+            {
+                TextCN.Visible = true;
+                ContainBx.Visible = false;
+            }
+            else
+            {
+                TextCN.Visible = false;
+                ContainBx.Visible = true;
+            }
+        }
+        #endregion
+
+    }
+}
